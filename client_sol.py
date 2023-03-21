a168c085a59d1be371e15d60953efbc4b935525f
cyberducks17 <65214787+cyberducks17@users.noreply.github.com>
2023-03-21 11:26:47 +0200

    Add files via upload

--- client_sol.py
+++ client_sol.py
@@ -10,88 +10,109 @@ SEND_BLOCK_SIZE = 8192
 FILE_READ_BLOCK = 4096
 
 
+# Handle the three way handshake (the start of the connection)
 def three_way_handshake(client_socket):
-    client_seq = random.randint(0, 1000)
-    data_to_send = {"SYN": 1, "SEQ": client_seq}
-    client_socket.send(str(data_to_send).encode())
+    client_seq = random.randint(0, 1000)  # Initialize the SEQ value for the client (in real life it can go up to 2.1B+)
+    data_to_send = {"SYN": 1, "SEQ": client_seq}  # Create dictionry with the values to send
+    client_socket.send(str(data_to_send).encode())  # Send first SYN to server
     client_seq += 1
 
-    data = client_socket.recv(SEND_BLOCK_SIZE).decode()
-    headers_and_data = json.loads(data.replace("'",'"'))
+    data = client_socket.recv(SEND_BLOCK_SIZE).decode()  # Recve first SYN ACK from server
+    # When dict int python use ' insted of " so in order that the json will work you need to change it to "
+    headers_and_data = json.loads(data.replace("'", '"'))
 
+    # Get the values of the flags in the packet the client had sent
     server_ack = int(headers_and_data["ACK"])
     server_seq = int(headers_and_data["SEQ"])
     server_syn = int(headers_and_data["SYN"])
 
-    client_ack = server_seq + server_syn
+    client_ack = server_seq + server_syn  # Update the client ack value.
 
-    data_to_send = {"ACK": client_ack, "SEQ": client_seq}
+    data_to_send = {"ACK": client_ack, "SEQ": client_seq}  # Create dictionry with the values to send.
     
-    client_socket.send(str(data_to_send).encode())
-    return client_ack, client_seq
+    client_socket.send(str(data_to_send).encode())  # Send back ACK to server and this way established the connection.
+    return client_ack, client_seq  # Return the client ack and seq for future packets.
 
 
+# Get the html data from server and as part of ex2 will also cut and decode it accordingly
 def recive_data_from_server(client_socket, client_ack, client_seq):
     html_data = ""
 
-    data_to_send = {"ACK": client_ack, "SEQ": client_seq, "DATA": "Hi SpongeBob SquarePants it Patrick!"}
-    client_socket.send(str(data_to_send).encode())
+    data_to_send = {"ACK": client_ack, "SEQ": client_seq, "DATA": "Hi SpongeBob SquarePants it Patrick!"}  # Create dictionry with the values to send
+    client_socket.send(str(data_to_send).encode())  # Send the request to the server.
 
-    client_seq += len(data_to_send["DATA"])
+    client_seq += len(data_to_send["DATA"])  # Update the client SEQ value
 
+    # Recve the ACK from the server to the request that he send.
+    # Becouse this sol is just poc it doas not check the values of the of ACK and SEQ.
+    # In a real sol they need to check the values and not trust the server.
     data = client_socket.recv(SEND_BLOCK_SIZE).decode()
     data = client_socket.recv(SEND_BLOCK_SIZE).decode()
+
+    # When dict int python use ' insted of " so in order that the json will work you need to change it to "
     headers_and_data = json.loads(data.replace("'", '"'))
 
+    # Get the values of the flags in the packet the client had sent
     server_ack = int(headers_and_data["ACK"])
     server_seq = int(headers_and_data["SEQ"])
     server_data = headers_and_data["DATA"]
     
-    client_ack += len(server_data)
+    client_ack += len(server_data)  # Update the client ACK value
 
+    # Part of the ex2 remove the string that single the start of the main data from the server to later decode it with base64
     server_data = server_data.replace("START\n", "")
     
+    # Run until the server dont have anymore to send know it by that that the data will end with the string END
     while "\nEND" not in server_data:
         
+        # Part of the ex2 decode the data with base64 to later save the html file and show it
         server_data = base64.b64decode(server_data).decode()
-        html_data += server_data
+        html_data += server_data  # Put togther the html data as part of ex2
         
-        data_to_send = {"ACK": client_ack, "SEQ": client_seq}
+        data_to_send = {"ACK": client_ack, "SEQ": client_seq}  # Create dictionry with the values to send
         client_socket.send(str(data_to_send).encode())
         
         data = client_socket.recv(SEND_BLOCK_SIZE).decode()
-        headers_and_data = json.loads(data.replace("'",'"'))
+        # When dict int python use ' insted of " so in order that the json will work you need to change it to "
+        headers_and_data = json.loads(data.replace("'", '"'))
 
+        # Get the values of the flags in the packet the client had sent
         server_ack = int(headers_and_data["ACK"])
         server_seq = int(headers_and_data["SEQ"])
         server_data = headers_and_data["DATA"]
 
         client_ack += len(server_data)
-
+    # Part of the ex2 remove the string that single the start of the main data from the server to later decode it with base64
     server_data = server_data.replace("\nEND", "")
+    # Part of the ex2 decode the data with base64 to later save the html file and show it
     server_data = base64.b64decode(server_data).decode()
-    html_data += server_data
+    html_data += server_data  # Put togther the html data as part of ex2
     
-    data_to_send = {"ACK": client_ack, "SEQ": client_seq}
+    data_to_send = {"ACK": client_ack, "SEQ": client_seq}  # Create dictionry with the values to send
     client_socket.send(str(data_to_send).encode())
 
+    # return the client seq and ack for future packets and the html data as part of ex2 
     return client_ack, client_seq, html_data
 
 
+# Handle the three way FIN handshake (the end of the connection)
 def fin_handshake(client_socket, client_ack, client_seq):
-    data_to_send = {"ACK": client_ack, "SEQ": client_seq, "FIN": 1}
+    data_to_send = {"ACK": client_ack, "SEQ": client_seq, "FIN": 1}  # Create dictionry with the values to send
     client_seq += 1
-    client_socket.send(str(data_to_send).encode())
+    client_socket.send(str(data_to_send).encode())  # Send the FIN flag to the server
 
-    data = client_socket.recv(SEND_BLOCK_SIZE).decode()
-    headers_and_data = json.loads(data.replace("'",'"'))
+    data = client_socket.recv(SEND_BLOCK_SIZE).decode()  # Receve the FIN flag from the server
+    # When dict int python use ' insted of " so in order that the json will work you need to change it to "
+    headers_and_data = json.loads(data.replace("'", '"'))
+
+    # Get the values of the flags in the packet the client had sent
     server_ack = int(headers_and_data["ACK"])
     server_seq = int(headers_and_data["SEQ"])
     server_FIN = int(headers_and_data["FIN"])
     client_ack += server_FIN
 
-    data_to_send = {"ACK": client_ack, "SEQ": client_seq}
-    client_socket.send(str(data_to_send).encode())
+    data_to_send = {"ACK": client_ack, "SEQ": client_seq}  # Create dictionry with the values to send
+    client_socket.send(str(data_to_send).encode())  # Send the finale ACK to the server to end the connection
     return client_ack, client_seq
 
 
@@ -106,11 +127,12 @@ def client_program():
     client_ack, client_seq, html_data = recive_data_from_server(client_socket, client_ack, client_seq)
     client_ack, client_seq = fin_handshake(client_socket, client_ack, client_seq)
 
-    data = client_socket.recv(SEND_BLOCK_SIZE).decode()
+    data = client_socket.recv(SEND_BLOCK_SIZE).decode()  # If all the discussion went smoothly the will recve ainput that passed the ex1
     print(data)
 
     client_socket.close()
 
+    # all the next code is for ex2 to show the input in the web browser.
     html_file = open("data.html", "w")
     html_file.write(html_data)
     html_file.close()
--- server.py
+++ server.py
@@ -7,46 +7,49 @@ import base64
 
 
 SEND_BLOCK_SIZE = 8192
-FILE_READ_BLOCK = 4096
+FILE_READ_BLOCK = SEND_BLOCK_SIZE / 2
 
 
+# Check if the client established a connection correctly.
 def check_syn_ack(conn):
-    data = conn.recv(SEND_BLOCK_SIZE).decode()
+    data = conn.recv(SEND_BLOCK_SIZE).decode()  # recv the input from the server
 
+    # In any case of error print the error and return 0, 0 to signle to close the connection
     try:
-        headers_and_data = json.loads(data.replace("'",'"'))
+        # When dict int python use ' insted of " so in order that the json will work you need to change it to "
+        headers_and_data = json.loads(data.replace("'", '"'))
 
-        syn_client = headers_and_data["SYN"]
-        seq_client = headers_and_data["SEQ"]
-
-        syn_client = int(syn_client)
-        seq_client = int(seq_client)
+        # Get the values of the flags in the packet the client had sent
+        syn_client = int(headers_and_data["SYN"])
+        seq_client = int(headers_and_data["SEQ"])
         
+        # If true then client had sent worng input so will close the connection
         if not syn_client == 1:
             raise Exception("SYN must be 1")
 
+        # If true then client had sent worng input so will close the connection
         if seq_client < 0:
             raise Exception("SEQ cant be negative")
 
-        seq_server = random.randint(0, 1000)
+        seq_server = random.randint(0, 1000)  # Initialize the SEQ value for the server (in real life it can go up to 2.1B+)
 
-        server_ack = seq_client + syn_client
+        server_ack = seq_client + syn_client  # Update the server ack value
 
-        data_to_send = {"SYN": 1, "SEQ": seq_server, "ACK": server_ack}
+        data_to_send = {"SYN": 1, "SEQ": seq_server, "ACK": server_ack}  # Create dictionry with the values to send
 
-        conn.send(str(data_to_send).encode())
+        conn.send(str(data_to_send).encode())  # Send the encoded data to server
         seq_server += 1
 
-        data = conn.recv(SEND_BLOCK_SIZE).decode()
+        data = conn.recv(SEND_BLOCK_SIZE).decode()  # recv the input from the server
 
+         # When dict int python use ' insted of " so in order that the json will work you need to change it to "
         headers_and_data = json.loads(data.replace("'",'"'))
 
-        ack_client = headers_and_data["ACK"]
-        seq_client = headers_and_data["SEQ"]
-        
-        ack_client = int(ack_client)
-        seq_client = int(seq_client)
+        # Get the values of the flags in the packet the client had sent
+        ack_client = int(headers_and_data["ACK"])
+        seq_client = int(headers_and_data["SEQ"])
 
+        # If true then client had sent worng input so will close the connection
         if not ack_client == seq_server:
             raise Exception("ACK must be the same as server SEQ")
 
@@ -58,36 +61,41 @@ def check_syn_ack(conn):
         return 0
     
 
+# Get the client request
 def get_client_request(conn, seq_server, server_ack):
-    data = conn.recv(SEND_BLOCK_SIZE).decode()
-    
+    data = conn.recv(SEND_BLOCK_SIZE).decode()  # recv the input from the server
+    # In any case of error print the error and return 0, 0 to signle to close the connection
     try:
-        headers_and_data = json.loads(data.replace("'",'"'))
+        # When dict int python use ' insted of " so in order that the json will work you need to change it to "
+        headers_and_data = json.loads(data.replace("'", '"'))
         
-        seq_client = headers_and_data["SEQ"]
-        ack_client = headers_and_data["ACK"]
+        # Get the values of the flags in the packet the client had sent
+        seq_client = int(headers_and_data["SEQ"])
+        ack_client = int(headers_and_data["ACK"])
 
-        ack_client = int(ack_client)
-        seq_client = int(seq_client)
-        
-        payload = headers_and_data["DATA"]
+        payload = headers_and_data["DATA"]  # Get the data part of the packet
 
+        # If true then client had sent worng input so will close the connection
         if not seq_client == server_ack:
             raise Exception("client SEQ must be the same as server ACK")
 
+        # If true then client had sent worng input so will close the connection
         if not seq_server == ack_client:
             raise Exception("client ACK must be the same as server SEQ")
 
-        server_ack += len(payload)
+        server_ack += len(payload)  # Update the server ack value
 
-        data_to_send = {"SEQ": seq_server, "ACK": server_ack}
-        conn.send(str(data_to_send).encode())
+        data_to_send = {"SEQ": seq_server, "ACK": server_ack}  # Create dictionry with the values to send
+        conn.send(str(data_to_send).encode())  # Send the packet to the client
 
+        # Check if the request from the client was correct and if not send him what the request should include,
+        # Use recursion call to recive agian the request from the client.
         if "Hi SpongeBob SquarePants it Patrick!" not in payload:
             data_to_send["DATA"] = "ERROR bad request!\nRequest must inculde 'Hi SpongeBob SquarePants it Patrick!'!"
-            seq_server += len(data_to_send["DATA"])
+            seq_server += len(data_to_send["DATA"])  # Update the server ack value
             
             conn.send(str(data_to_send).encode())
+            # Use recursion call to recive agian the request from the client.
             seq_server, server_ack = get_client_request(conn, seq_server, server_ack)
              
         return seq_server, server_ack
@@ -98,66 +106,76 @@ def get_client_request(conn, seq_server, server_ack):
         return 0, 0
 
 
+# send the client the correct info
 def send_data_to_client(conn, seq_server, server_ack, html_path):
+    # In any case of error print the error and return 0, 0 to signle to close the connection
     try:
-        data_to_send = {"SEQ": seq_server, "ACK": server_ack, "DATA": "START\n"}
+        data_to_send = {"SEQ": seq_server, "ACK": server_ack, "DATA": "START\n"}  # Create dictionry with the values to send
 
+        # Read the input html file to send it in segmants to the client
         html_file = open(html_path, "r")
-        html_data = html_file.read(FILE_READ_BLOCK)
+        html_data = html_file.read(FILE_READ_BLOCK)  # Read the file in blocks
+        # Run while there is still data to read
         while html_data:
-            
+            # Encode the data to send with base64
             data_to_send["DATA"] = "{}{}".format(data_to_send["DATA"], base64.b64encode(html_data.encode()).decode())
 
+            # When reach the last block of the file add END to the data so the client will now to send FIN
             if len(html_data) < FILE_READ_BLOCK:
                 data_to_send["DATA"] = "{}\nEND".format(data_to_send["DATA"])
             
             conn.send(str(data_to_send).encode())
 
-            seq_server += len(data_to_send["DATA"])
+            seq_server += len(data_to_send["DATA"])  # Update the server seq value
 
-            data = conn.recv(SEND_BLOCK_SIZE).decode()
-            headers_and_data = json.loads(data.replace("'",'"'))
+            data = conn.recv(SEND_BLOCK_SIZE).decode()  # recv the input from the server
+            # When dict int python use ' insted of " so in order that the json will work you need to change it to "
+            headers_and_data = json.loads(data.replace("'", '"'))
 
-            seq_client = headers_and_data["SEQ"]
-            ack_client = headers_and_data["ACK"]
+            seq_client = int(headers_and_data["SEQ"])
+            ack_client = int(headers_and_data["ACK"])
 
-            ack_client = int(ack_client)
-            seq_client = int(seq_client)
-
+            # If true then client had sent worng input so will close the connection
             if not seq_client == server_ack:
                 raise Exception("client SEQ must be the same as server ACK")
 
+            # If true then client had sent worng input so will close the connection
             if not seq_server == ack_client:
                 raise Exception("client ACK must be the same as server SEQ")
 
-            html_data = html_file.read(FILE_READ_BLOCK)
-            data_to_send = {"SEQ": seq_server, "ACK": server_ack, "DATA": ""}
+            html_data = html_file.read(FILE_READ_BLOCK)  # Read the file in blocks
+            data_to_send = {"SEQ": seq_server, "ACK": server_ack, "DATA": ""}  # Create dictionry with the values to send
 
-        data = conn.recv(SEND_BLOCK_SIZE).decode()
+        data = conn.recv(SEND_BLOCK_SIZE).decode()  # recv the input from the server
         
-        headers_and_data = json.loads(data.replace("'",'"'))
+        # When dict int python use ' insted of " so in order that the json will work you need to change it to "
+        headers_and_data = json.loads(data.replace("'", '"'))
 
         fin_client = headers_and_data["FIN"]
         ack_client = headers_and_data["ACK"]
 
+        # If true then client had sent worng input so will close the connection
         if not fin_client == 1:
             raise Exception("client must set FIN flag to 1")
 
+        # If true then client had sent worng input so will close the connection
         if not seq_server == ack_client:
             raise Exception("client ACK must be the same as server SEQ")
         
         server_ack += fin_client
-        data_to_send = {"SEQ": seq_server, "ACK": server_ack, "FIN": 1}
+        data_to_send = {"SEQ": seq_server, "ACK": server_ack, "FIN": 1}  # Create dictionry with the values to send
         conn.send(str(data_to_send).encode())
 
         seq_server += 1
 
-        data = conn.recv(SEND_BLOCK_SIZE).decode()
+        data = conn.recv(SEND_BLOCK_SIZE).decode()  # recv the input from the server
         
-        headers_and_data = json.loads(data.replace("'",'"'))
+        # When dict int python use ' insted of " so in order that the json will work you need to change it to "
+        headers_and_data = json.loads(data.replace("'", '"'))
 
         ack_client = headers_and_data["ACK"]
 
+        # If true then client had sent worng input so will close the connection
         if not seq_server == ack_client:
             raise Exception("client ACK must be the same as server SEQ")
 
@@ -171,21 +189,25 @@ def send_data_to_client(conn, seq_server, server_ack, html_path):
 
 def serve(conn, html_file_path):
     seq_server, server_ack = check_syn_ack(conn)
-        
+    
+    # If true then a error occured so will close the connection and exit
     if not seq_server:
-        conn.close()
+        conn.close()  # close the connection
         return
 
+    # If true then a error occured so will close the connection and exit
     seq_server, server_ack = get_client_request(conn, seq_server, server_ack)
     if not seq_server:
-        conn.close()
+        conn.close()  # close the connection
         return
 
+    # If true then a error occured so will close the connection and exit
     seq_server, server_ack = send_data_to_client(conn, seq_server, server_ack, html_file_path)
     if not seq_server:
-        conn.close()
+        conn.close()  # close the connection
         return
 
+    # If the client recved this masseage then he manged to send all packets with the correct falgs value and finshed layer 4
     conn.send("Well done agent Patrick you have passed layer 4 exercise".encode())
 
     conn.close()  # close the connection
@@ -212,10 +234,10 @@ def server_program():
 
     while True:
         try:
-            conn, address = server_socket.accept()  # accept new connection
-            print("Connection from: " + str(address))
+            conn, address = server_socket.accept()  # Accept new connection
+            print("Connection from: " + str(address))  # Print new connection
 
-            thread = threading.Thread(target=serve, args=(conn, html_file_path))
+            thread = threading.Thread(target=serve, args=(conn, html_file_path))  # Let a thread handle the new connection
             thread.start()
         except Exception as err:
             print(err)
